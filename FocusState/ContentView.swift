58eb71317838fcb88f566b0576804c68cfe8529f
Federico <federico@fastmail.com>
2022-05-10 16:44:21 +0200

    Initial Commit

---
+++ FocusState/ContentView.swift
@@ -0,0 +1,53 @@
+//
+//  ContentView.swift
+//  FocusState
+//
+//  Created by Federico on 10/05/2022.
+//
+
+import SwiftUI
+
+enum FocusText {
+    case username, password
+}
+
+struct ContentView: View {
+    @FocusState private var focusField: FocusText?
+    @State private var usernameText = ""
+    @State private var passwordText = ""
+    @State private var submittedText = "This will display your credentials."
+    
+    var body: some View {
+        Form() {
+            Section("Credentials") {
+                TextField("Enter a username", text: $usernameText)
+                    .focused($focusField, equals: .username)
+                    .submitLabel(.next)
+                    .onSubmit {
+                        focusField = .password
+                    }
+                TextField("Enter a password", text: $passwordText)
+                    .focused($focusField, equals: .password)
+                    .submitLabel(.done)
+                    .onSubmit {
+                        submitData()
+                    }
+            }
+            Section("Submitted") {
+                Text(submittedText)
+            }
+        }
+        
+    }
+    
+    func submitData() {
+        focusField = nil
+        submittedText = usernameText + "," + passwordText
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ FocusState/FocusStateApp.swift
@@ -0,0 +1,17 @@
+//
+//  FocusStateApp.swift
+//  FocusState
+//
+//  Created by Federico on 10/05/2022.
+//
+
+import SwiftUI
+
+@main
+struct FocusStateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
